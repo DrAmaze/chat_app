6d2712ea6fd22c17e04120746c6126ac706b9aa8
jacob <jbutler8@terpmail.umd.edu>
2018-06-29 16:09:10 -0700

    socket is open

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -2,6 +2,8 @@ document.addEventListener('DOMContentLoaded', () => {
   const socket = require('socket.io-client')();
   const myChat = require('./chatUI');
 
+  console.log('here', socket);
+
   setInterval(() => {
     socket.emit('rooms');
   }, 1000);
