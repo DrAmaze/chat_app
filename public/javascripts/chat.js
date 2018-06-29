9dcf2198e6f32852c40760a7ebcdc5ca07858eae
jacob <jbutler8@terpmail.umd.edu>
2018-06-29 16:51:10 -0700

    add process command function to chat

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -3,6 +3,7 @@ class Chat {
     this.socket = socket;
     this.sendMessage = this.sendMessage.bind(this);
     this.changeRoom = this.changeRoom.bind(this);
+    this.processCommand = this.processCommand.bind(this);
   }
 
   sendMessage(room, msg) {
@@ -11,6 +12,29 @@ class Chat {
 
   changeRoom(room) {
     this.socket.emit('join', { newRoom: room });
+  }
+
+  processCommand(command) {
+    const words = command.split(' ');
+    const parsedCmd = words[0].substring(1, words[0].length).toLowerCase();
+    let msg = false;
+
+    switch (parsedCmd) {
+      case 'join':
+        words.shift();
+        const room = words.join(' ');
+        this.changeRoom(room);
+        break;
+      case 'nick':
+        words.shift();
+        const name = words.join(' ');
+        this.socket.emit('nameAttempt', name);
+        break;
+      default:
+        msg = 'Unrecognized command.';
+        break;
+    }
+    return msg;
   }
 }
 
